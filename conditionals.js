7e0a19cc0455425afdf26cf8925c470d31263efa
Sourav <souravbadhan909@gmail.com>
2023-03-05 01:49:44 -0500

    use of conditional chorus

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,21 @@
-const raining =true;
-if (!raining) {
-  console.log("Leave your umbrella at home!");
+function whichSchool(age){
+  if (age < 13){
+    console.log ("Elementary School");
+  }
+  else if(age >= 13 && age <= 18){
+    console.log("Secondary School")
+  }
+  else{ 
+    console.log("LightHouse Labs");
+  }
 }
+}
+const whichSchool = function (age) {
+  if (age < 13) {
+    return 'Elementary School';
+  } else if (age >= 13 && age <= 18) {
+    return 'Secondary School';
+  } else {
+    return 'Lighthouse Labs';
+  }
+};
